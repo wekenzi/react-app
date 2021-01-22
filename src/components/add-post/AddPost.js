c9767c727f4590cd7085c59f7ffe0f9c3e9246c0
Mohamed Khaled <mohamed.khaled@nabdacare.com>
2021-01-22 16:07:43 +0200

    Add Post Form Validations

--- src/components/add-post/AddPost.js
+++ src/components/add-post/AddPost.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from'./CardStyle.module.css';
+import { useForm } from "react-hook-form";
 
-function AddPost() {
+const AddPost = () => {
+
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
         <div className={styles.wrapperCard}>
             <div className="card">
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control" id="name" />
+                            <input type="text" className="form-control" name="name" id="name" ref={register({ required: true })}/>
+                            {errors.name && <small className="text-danger">Type name</small>}
                         </div>
                         <div className="form-group">
                             <label htmlFor="content">Content</label>
-                            <textarea className="form-control" id="content" rows="3"></textarea>    
+                            <textarea className="form-control" id="content" name="content" rows="3" ref={register({ required: true })}></textarea>
+                            {errors.content && <small className="text-danger">Type content</small>}
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
