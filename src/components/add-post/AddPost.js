53e14ffdfde1cd9993fae9cbb6da4878934a555f
Mohamed Khaled <mohamed.khaled@nabdacare.com>
2021-01-22 14:58:35 +0200

    add post layout

--- src/components/add-post/AddPost.js
+++ src/components/add-post/AddPost.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styles from'./CardStyle.module.css';
 
 function AddPost() {
     return (
-        <div className="mt-3">
-            <form>
-                <h1>
-                    Iam form
-                </h1>
-            </form>
+        <div className={styles.wrapperCard}>
+            <div className="card">
+                <div className="card-body">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id="name" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="content">Content</label>
+                            <textarea className="form-control" id="content" rows="3"></textarea>    
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
