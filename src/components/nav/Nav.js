c1c97fea08f115d3b37d01156d3f983ebc6a7c31
Mohamed Khaled <wekenzi1995@gmail.com>
2021-01-30 23:17:38 +0200

    some edits

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,57 +2,36 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import './NavStyle.css';
 import logo from "../../assets/images/logo.png";
-import pic1 from "../../assets/images/calendar.svg";
 import pic2 from "../../assets/images/findDoc.png";
 import pic3 from "../../assets/images/mediSrv.png";
 import pic4 from "../../assets/images/heaTip.png";
 import pic5 from "../../assets/images/treatment.png";
 import pic6 from "../../assets/images/video-icon.png";
 
+const Links = [
+    {id:1, label:'Home Visit', route:'/homevisit', img:pic2},
+    {id:2, label:'Nearby Medical Services', route:'/nms', img:pic3},
+    {id:3, label:'Health Tips', route:'/healthtips', img:pic4},
+    {id:4, label:'Cross Border Treatment', route:'/cbt', img:pic5},
+    {id:5, label:'Tele Medicine', route:'/telemedicine', img:pic6},
+];
+
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand py-0">
+        <nav className="navbar navbar-expand py-0 sticky-top">
             <div className="collapse navbar-collapse" id="navbarNav">
                 <NavLink className="navbar-brand" to="/" exact>
                     <img src={logo} alt="img"/>
                 </NavLink>
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <NavLink to="/" exact className="nav-link">
-                            <img src={pic1} alt="img"/>
-                            <span>Booking Appointment</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/addpost" exact className="nav-link">
-                            <img src={pic2} alt="img"/>
-                            <span>Home Visit</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/" exact className="nav-link">
-                            <img src={pic3} alt="img"/>
-                            <span>Nearby Medical Services</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/addpost" exact className="nav-link">
-                            <img src={pic4} alt="img"/>
-                            <span>Health Tips</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/" exact className="nav-link">
-                            <img src={pic5} alt="img"/>
-                            <span>Cross Border Treatment</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/addpost" exact className="nav-link">
-                            <img src={pic6} alt="img"/>
-                            <span>Tele Medicine</span>
-                        </NavLink>
-                    </li>
+                    {Links.map(link=>
+                        <li className="nav-item" key={link.id}>
+                            <NavLink to={link.route} exact className="nav-link">
+                                <img src={link.img} alt="img"/>
+                                <span>{link.label}</span>
+                            </NavLink>
+                        </li>
+                    )}
                 </ul>
             </div>
         </nav>
